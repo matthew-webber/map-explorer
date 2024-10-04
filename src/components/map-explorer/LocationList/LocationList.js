48c1e48767b15c093233822ca5d5621b47eac99f
Matt Webber <dev.matthew.lee.webber@gmail.com>
2024-10-04 17:49:56 -0400

    adds working location list rendering

--- src/components/map-explorer/LocationList/LocationList.js
+++ src/components/map-explorer/LocationList/LocationList.js
@@ -1,13 +1,30 @@
+import { store } from '../../../store/store'; // Import the store
+
 class LocationList {
     constructor() {
         this.listContainer = document.getElementById('location-list');
+        this.subscribeToStore(); // Initialize the store subscription
     }
+
+    subscribeToStore = () => {
+        store.subscribe(() => {
+            const locations = store.getState().locations.data;
+            // log all the properties of locations
+            for (var key in locations) {
+                console.log(
+                    `asdf locations key ${key} value ${locations[key]}`
+                );
+            }
+            this.updateList(locations);
+        });
+    };
 
     updateList(locations) {
         this.listContainer.innerHTML = ''; // Clear existing entries
-        locations.forEach(location => {
+        locations.forEach((location) => {
             const listItem = document.createElement('div');
-            listItem.textContent = location.name; // Assuming 'name' is a property of location
+            listItem.classList.add('location-item');
+            listItem.textContent = location.locationName; // Assuming 'name' is a property of location
             this.listContainer.appendChild(listItem);
         });
     }
